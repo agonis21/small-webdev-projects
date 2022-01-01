568d0cf6e0c9118f48b86451d411cf58feb0d9b9
Asm <asmgoni@protonmail.com>
2022-01-01 12:02:43 -0500

    a working progress bar with buttons

--- GoalProgressBar/script.js
+++ GoalProgressBar/script.js
@@ -1,13 +1,44 @@
 window.onload = function () {
+    addSpanValues();
+    addButtonFunctionality();
+}
+
+function addSpanValues() {
     //get the goal divs
     var goals = document.querySelectorAll(".goal");
 
-    
+    goals.forEach(function (goal) {
+        //get the value of progress bar
+        var pval = goal.querySelector("progress").value;
 
+        //span element will be added to label
+        var label = goal.querySelector("label");
+
+        //add span elements
+        label.innerHTML = "<span>" + pval + "</span>" +
+            " " + label.innerHTML.toLowerCase();
+    });
+}
+function updateValues() {
+    //get the goal divs
+    var goals = document.querySelectorAll(".goal");
+
+    goals.forEach(function (goal) {
+        //get the value of progress bar
+        var pval = goal.querySelector("progress").value;
+
+        //get the span element and change value 
+        var span = goal.querySelector("span");
+        span.innerText = pval;
+    });
+}
+function addButtonFunctionality() {
+    //get the goal divs
+    var goals = document.querySelectorAll(".goal");
+
+    //referenced: https://stackoverflow.com/questions/750486/javascript-closure-inside-loops-simple-practical-example
     //loop through each of the goal
-    for (var i = 0; i < goals.length; goals++) {
-        var goal = goals[i];
-
+    goals.forEach(function (goal) {
         //get the progressbar
         var pbar = goal.querySelector("progress");
 
@@ -23,12 +54,13 @@ window.onload = function () {
         //add functionality to the buttons
         increase_btn.addEventListener("click", function () {
             pbar.value += increase_val;
+            updateValues();
         }, false);
 
         decrease_btn.addEventListener("click", function () {
             pbar.value += decrease_val;
+            updateValues();
         }, false);
 
-        //fix event listener bug
-    }
+    });
 }
